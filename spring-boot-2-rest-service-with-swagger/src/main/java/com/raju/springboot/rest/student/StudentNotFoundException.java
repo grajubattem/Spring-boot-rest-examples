1622a5d1a3e59b8322d3ca3c7fef24bd40e49cb7
Gangaraju Battem(Raju) <gangaraju.battem@gmail.com>
2018-10-21 08:56:49 +0530

    swagger example

---
+++ spring-boot-2-rest-service-with-swagger/src/main/java/com/raju/springboot/rest/student/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package com.raju.springboot.rest.student;
+
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException(String exception) {
+		super(exception);
+	}
+
+}
